c05b284776d2f994bf21d9b280bfe2948dd43987
daiter gd <daitergod19@gmail.com>
2024-11-25 08:27:49 +0300

    init circle

---
+++ bug.lua
@@ -0,0 +1,38 @@
+printTable = function(t)
+	for pos, val in pairs(t) do
+		print("[" .. tostring(pos) .. "] => " .. tostring(val))
+	end
+end
+printTableRecursive = function(t)
+	local indent = "Table: "
+	local printTable_cache = {}
+	local function sub_printTable(t, indent)
+		if printTable_cache[tostring(t)] then
+			print(indent .. "*" .. tostring(t))
+		else
+			printTable_cache[tostring(t)] = true
+			if type(t) == "table" then
+				for pos, val in pairs(t) do
+					local poss = tostring(pos)
+					if type(pos) == "table" then
+						print(indent .. "[" .. poss .. "] => " .. tostring(t) .. " {")
+						sub_printTable(pos, indent .. string.rep(" ", string.len(poss) + 8))
+						print(indent .. string.rep(" ", string.len(poss) + 6) .. "}")
+					end
+					if type(val) == "table" then
+						print(indent .. "[" .. poss .. "] => " .. tostring(t) .. " {")
+						sub_printTable(val, indent .. string.rep(" ", string.len(poss) + 8))
+						print(indent .. string.rep(" ", string.len(poss) + 6) .. "}")
+					elseif type(val) == "string" then
+						print(indent .. "[" .. poss .. '] => "' .. val .. '"')
+					else
+						print(indent .. "[" .. poss .. "] => " .. tostring(val))
+					end
+				end
+			else
+				print(indent .. tostring(t))
+			end
+		end
+	end
+	sub_printTable(t, "")
+end
---
+++ main.lua
@@ -0,0 +1,34 @@
+KeyPressed = {}
+KeyReleased = {}
+Update = {}
+
+local draw = require("draw")
+---@class love
+local love = require("love")
+require("bug")
+
+function love.load()
+	draw:load()
+end
+
+function love.draw()
+	draw:draw()
+end
+
+function love.keypressed(key, unicode)
+	for _, fn in pairs(KeyPressed) do
+		fn(key, unicode)
+	end
+end
+
+function love.keyreleased(key)
+	for _, fn in pairs(KeyReleased) do
+		fn(key)
+	end
+end
+
+function love.update(dt)
+	for _, fn in pairs(Update) do
+		fn(dt)
+	end
+end
